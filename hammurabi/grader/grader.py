fb637f00fd0574ef22b6b74bdca2a1ab5415f3de
Yuriy Guts <yuriy.guts@gmail.com>
2015-07-17 13:32:21 +0300

    Fixed a bug occurring with absolute paths in problem/report root.

--- hammurabi/grader/grader.py
+++ hammurabi/grader/grader.py
@@ -89,14 +89,14 @@ def get_problem_root_dir(config):
     problem_root_dir = config.get_safe("locations/problem_root", default_value="problems")
     if not os.path.isabs(problem_root_dir):
         problem_root_dir = os.path.join(os.path.dirname(__file__), problem_root_dir)
-        return problem_root_dir
+    return problem_root_dir
 
 
 def get_report_root_dir(config):
     report_root_dir = config.get_safe("locations/report_root", default_value="reports")
     if not os.path.isabs(report_root_dir):
         report_root_dir = os.path.join(os.path.dirname(__file__), report_root_dir)
-        return report_root_dir
+    return report_root_dir
 
 
 def get_report_output_dir(config):
