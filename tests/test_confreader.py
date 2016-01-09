f0e4344a6f128ce940723eb7b5b9583fcfb9e779
Yuriy Guts <yuriy.guts@gmail.com>
2016-01-09 21:58:03 +0200

    Added tests for utility modules.

---
+++ tests/test_confreader.py
@@ -0,0 +1,71 @@
+import pytest
+import hammurabi.utils.confreader as confreader
+from hammurabi.utils.dictview import ObjectDictView
+
+
+sample_config_content = """
+{
+    "locations": {
+        "problem_root": "problems",
+        "report_root": "reports"
+    }
+}
+"""
+
+invalid_config_content = """
+{
+    "locations": }}}
+}
+"""
+
+
+@pytest.fixture()
+def filename_for_sample_valid_config(tmpdir):
+    file_path = tmpdir.join("config.conf")
+    filename = file_path.strpath
+
+    with open(filename, "w") as sample_config:
+        sample_config.write(sample_config_content)
+
+    return filename
+
+
+@pytest.fixture()
+def filename_for_invalid_config(tmpdir):
+    file_path = tmpdir.join("config_invalid.conf")
+    filename = file_path.strpath
+
+    with open(filename, "w") as invalid_config:
+        invalid_config.write(invalid_config_content)
+
+    return filename
+
+
+@pytest.fixture()
+def non_existent_filename(tmpdir):
+    return tmpdir.join("i_do_not_exist.conf").strpath
+
+
+def test_read_config_given_invalid_json_throws(filename_for_invalid_config):
+    # Assert
+    with pytest.raises(Exception):
+        config = confreader.read_config(filename_for_invalid_config)
+
+
+def test_read_config_given_nonexistent_file_returns_blank_object(non_existent_filename):
+    # Act
+    config = confreader.read_config(non_existent_filename)
+
+    # Assert
+    assert isinstance(config, ObjectDictView)
+    assert len(vars(config)) == 0
+
+
+def test_read_config_given_valid_json_returns_nested_objects(filename_for_sample_valid_config):
+    # Act
+    config = confreader.read_config(filename_for_sample_valid_config)
+
+    # Assert
+    assert len(vars(config)) == 1
+    assert config.locations.problem_root == "problems"
+    assert config.locations.report_root == "reports"
